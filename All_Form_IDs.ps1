332b47635add83902d05b7fc24c85f48ef0d9b0f
aingelsten <aingelsten@gmail.com>
2023-01-29 10:38:18 +0000

    Minor update to form list ID

--- All_Form_IDs.ps1
+++ All_Form_IDs.ps1
@@ -16,12 +16,6 @@ $FVApiForms = New-WebServiceProxy -Uri "https://www.priority1.uk.net/FieldViewWe
 
 Write-Output "Getting Project ID's"
 
-$FVApiConfig.GetProjects($apiToken, $null, $null, 1, 0, 100).ProjectInformation.childnodes.id
-
-
-$id = $FVApiConfig.GetProjects($apiToken, $null, $null, 1, 0, 100).ProjectInformation.childnodes.id
-
-
 $id | Out-File c:\Users\aingelsten\scripts\projectid.txt
 
 Write-Output "Getting Dates"
@@ -32,9 +26,15 @@ Write-Output $reportEnd
 
 $datefrom = Get-Date -Date $reportEnd.AddMonths(-2) -Format "yyyy-MM-ddTHH:mm:ss"
 
+Write-Output "Date From"
+Write-Output $datefrom
+
 $dateTo= $reportEnd
 
-$projectids = Get-Content C:\Users\aingelsten\scripts\projectid.txt
+Write-Output "Date To"
+Write-Output $dateTo
+
+$projectids = Get-Content C:\Users\aingelsten\scripts\projectid2.txt
 
 Write-Output "Starting loop"
 
@@ -44,12 +44,12 @@ foreach ($projectid in $projectids)
 
 Write-Output $projectid 
 
-$FVApiForms.GetProjectFormsList($apiToken, $projectid, $null, 0, $datefrom, $dateTo, $null, $null, $null, $null, $null, $null).ProjectFormsListInformation.childnodes
+$FVApiForms.GetProjectFormsList($apiToken, $projectid, $null, 0, $datefrom, $dateTo, $null, $null, $null, $null, $null, $null).ProjectFormsListInformation.childnodes.FormID
 
 $formsListID = $FVApiForms.GetProjectFormsList($apiToken, $projectid, $null, 0, $datefrom, $dateTo, $null, $null, $null, $null, $null, $null).ProjectFormsListInformation.childnodes.FormID
 
 
-if ($formsListID -eq $null)
+if ($null -eq $formsListID)
 
 {
 Write-Output "*****NOTHING FOUND****"
@@ -61,13 +61,46 @@ else
 #
 #$formsList | Add-Member -MemberType NoteProperty -Name "ProjectId" -Value $projectid
 
-#Write-Output  "Writing to file"
+Write-Output  "Writing to file"
 
 Write-Output $formsListID
 
-#Write-Output  "Data written to file"
+$str_list = @($formsListID)
+$obj_list = $str_list | Select-Object @{Name='Form_ID';Expression={$_}}
+$obj_list | Export-Csv -Path c:\Users\aingelsten\scripts\formslist_ID.csv -append -NoType
+
+#fix https://stackoverflow.com/questions/19450616/export-csv-exports-length-but-not-name
+
+Write-Output  "Data written to file"
+
 }
 
 Start-Sleep -Seconds 10
 
 }
+
+Start-Sleep -Seconds 0.5
+
+Write-Output "Removing duplicates"
+
+$cleaned = Import-Csv C:\Users\aingelsten\scripts\formslist_ID.csv| Sort-Object Form_ID –Unique
+
+Start-Sleep -Seconds 0.5
+
+$cleaned | Export-Csv -Path C:\Users\aingelsten\scripts\formslist_ID.csv -NoTypeInformation
+
+Write-Output "Exporting to SharePoint"
+
+#Configuration of Sharepoint Variables
+$SiteURL = "https://typetecmg.sharepoint.com/sites/ITMainline"
+$SourceFilePath ="c:\Users\aingelsten\scripts\formslist.csv"
+$DestinationPath = "Kpi_Data" #Site Relative Path of the Library
+  
+  
+#Connect to PnP Online using Weblogin
+Connect-PnPOnline -Url $SiteURL -UseWebLogin  
+
+#Powershell pnp to upload file to sharepoint online
+Add-PnPFile -Path $SourceFilePath -Folder $DestinationPath
+
+Write-Output "Process Completed"
