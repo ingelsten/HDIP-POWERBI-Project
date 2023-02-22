141016a825ad24fd0dc9e1c7e6f60b4a46875f13
aingelsten <aingelsten@gmail.com>
2023-02-22 10:31:48 +0000

    Updates to scripts for FV answers

--- FV_TimesSheet_Answers.ps1
+++ FV_TimesSheet_Answers.ps1
@@ -15,16 +15,17 @@ $answer = "FVTimeSheet_Completed_By.csv"
 $answer1 = "FVTimeSheet_Contract.csv"
 $answer2 = "FVTimeSheet_Date.csv"
 $answer3 = "FVTimeSheet_Hours.csv"
-$questionAlias = "Completed By"
-$questionAlias1 = "Contract"
-$questionAlias2 = "Date"
-$questionAlias3 = "Hour Calculation"
+$questionAlias = "TS_Completed_By"
+$questionAlias1 = "TS_Contract"
+$questionAlias2 = "TS_Date_Timesheet"
+$questionAlias3 = "TS_Hour_Calculation"
 
 
 #Gets All ID's
-$formIds = Get-Content "C:\Users\aingelsten\scripts\All_Coins_Timesheet_ID.csv"
+$formIds = Get-Content "C:\Users\aingelsten\scripts\All_Coins_Timesheet_ID.txt"
+#$formIds = Get-Content "C:\Users\aingelsten\scripts\All_Coins_Timesheet_ID.csv"
 #$formIds = Get-Content "C:\Users\aingelsten\scripts\All_Formslist_ID.csv"
-#$formIds = "F248159.193"
+#$formIds = "F207755.375"
 
 #Loops through every project id
 foreach ($FormID in $formIds)
