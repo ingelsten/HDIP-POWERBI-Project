4308767526269d942685fac2362b56442f14c342
aingelsten <aingelsten@gmail.com>
2023-03-07 14:41:20 +0000

    Document update, started looking into PDF export

---
+++ PDF_EXPORT.ps1
@@ -0,0 +1,22 @@
+﻿
+
+#gets API token from file
+$ApiToken = Get-Content C:\Users\aingelsten\scripts\api_AF_id.txt
+$formid = "F1.1826236"
+$bolean ="Y"
+$size = "Large"
+
+Write-Output "Connecting to API"
+
+#calls the API
+$FVApiForms = New-WebServiceProxy -Uri "https://www.priority1.uk.net/FieldViewWebServices/WebServices/XML/API_FormsServices.asmx?WSDL"
+
+#Getting data by created date
+$FVApiForms.GetFormPDF($apiToken, $formid, $bolean, $bolean, $bolean, $bolean, $bolean, $bolean, $size).ProjectFormsListResponse
+
+
+$response = $FVApiForms.GetFormPDF($apiToken, $formid, $bolean, $bolean, $bolean, $bolean, $bolean, $bolean, $size).ProjectFormsListResponse
+
+Write-Output $response
+
+Write-Output "finished"
